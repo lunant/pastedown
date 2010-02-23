618cf4e4efaabb1a6d70faff0666a3f8a8c1232e
dahlia <dahlia@lunant.net>
2010-02-24 02:19:15 +0900

    Implemented basic functions.

--- pastedown/app.py
+++ pastedown/app.py
@@ -1,15 +1,40 @@
+import os.path
 from google.appengine.ext import webapp
+from google.appengine.ext import db
+import jinja2
 from pastedown.model import *
+
+
+VIEW_PATH = os.path.join(os.path.dirname(__file__), "views")
+VIEW_LOADER = jinja2.FileSystemLoader(VIEW_PATH)
+VIEW_ENV = jinja2.Environment(loader=VIEW_LOADER)
 
 
 class HomeHandler(webapp.RequestHandler):
 
     def get(self):
-        for doc in Document.all():
-            print>>self.response.out, doc.html
+        docs = Document.all().filter("updated_at != ", None) \
+                             .order("-updated_at")
+        template = VIEW_ENV.get_template("home.html")
+        self.response.out.write(template.render(documents=docs))
+
+    def post(self):
+        """Post a new document."""
+        doc = Document()
+        doc.put()
+        doc.body = self.request.get("body")
+        self.redirect("/" + str(doc.key()))
+
+
+class DocumentHandler(webapp.RequestHandler):
+
+    def get(self, key):
+        document = Document.get(db.Key(key))
+        self.response.out.write(document.html)
 
 
 application = webapp.WSGIApplication([
-    ("/", HomeHandler)
+    ("/", HomeHandler),
+    ("/(?P<key>[^~/][^/]*)", DocumentHandler)
 ], debug=True)
 
--- pastedown/model.py
+++ pastedown/model.py
@@ -1,3 +1,5 @@
+import re
+import htmlentitydefs
 from google.appengine.ext import db
 import markdown2
 import vlaah
@@ -20,6 +22,15 @@ def vlaah_session(file=None):
 
 VLAAH = vlaah_session()
 MARKDOWN = markdown2.Markdown(extras=["footnotes"])
+
+
+def strip_html(html):
+    html = re.sub(ur"<[^>]+>", u"", html)
+    map = htmlentitydefs.name2codepoint
+    return re.sub(ur"&(?:#([0-9a-fA-F]+)|(\w+));",
+                  lambda m: unichr(map[m.group(2)] if m.group(2)
+                                                   else int(m.group(1), 16)),
+                  html)
 
 
 class Document(db.Model):
@@ -54,12 +65,19 @@ class Document(db.Model):
         body = self.current_revision
         return body and body.html
 
+    @property
+    def title(self):
+        body = self.current_revision
+        return body and body.title
+
     def __unicode__(self):
-        return self.body
+        return self.title or u""
 
 
 class Revision(db.Model):
     """Document revisions."""
+
+    TITLE_PATTERN = re.compile(ur"<h1(\s[^>]*)?>(?P<title>.+?)</h1>")
 
     document = db.ReferenceProperty(Document, required=True,
                                     collection_name="revisions", indexed=True)
@@ -71,15 +89,26 @@ class Revision(db.Model):
         """Returns the HTML string of its body."""
         return self.body and MARKDOWN.convert(self.body)
 
+    @property
+    def title(self):
+        """Title of the document. It is from its first <h1> text or its first
+        some text.
+
+        """
+        match = self.TITLE_PATTERN.search(self.html)
+        if match:
+            return strip_html(match.group("title"))
+        return strip_html(self.html)[:80]
+
     def put(self):
         def put_it():
-            db.Model.put(self)
             doc = self.document
             doc.updated_at = self.created_at
             doc.put()
+            db.Model.put(self)
         db.run_in_transaction(put_it)
         return self.key()
 
     def __unicode__(self):
-        return unicode(self.body)
+        return unicode(self.body) or u""
 
